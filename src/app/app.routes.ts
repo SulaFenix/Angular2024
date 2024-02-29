ff2bbfe8d714a9e42a808d456b7316bf3285684a
Suellen Gularte <sula.fenix@gmail.com>
2024-02-28 22:39:37 -0300

    Aula 16_02

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ListagemUsuariosComponent } from './pages/usuarios/listagem/listagem.component';
+import { ListagemProdutosComponent } from './pages/produtos/listagem/listagem.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'usuarios', component: ListagemUsuariosComponent },
+  { path: 'produtos', component: ListagemProdutosComponent },
+];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
---
+++ src/app/pages/usuarios/listagem/listagem.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListagemUsuariosComponent } from './listagem.component';
+
+describe('ListagemComponent', () => {
+  let component: ListagemUsuariosComponent;
+  let fixture: ComponentFixture<ListagemUsuariosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ListagemUsuariosComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ListagemUsuariosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
