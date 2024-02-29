eb6284f22852d3b857d99acdab2b5e70cd6bed6b
Suellen Gularte <sula.fenix@gmail.com>
2024-02-28 23:25:34 -0300

    Exercício 16_02

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ListagemUsuariosComponent } from './pages/usuarios/listagem/listagem.component';
 import { ListagemProdutosComponent } from './pages/produtos/listagem/listagem.component';
+import { CadastroEdicaoComponent } from './pages/produtos/cadastro-edicao/cadastro-edicao.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'usuarios', component: ListagemUsuariosComponent },
+  { path: 'cadastroUsuario', component: CadastroEdicaoComponent},
   { path: 'produtos', component: ListagemProdutosComponent },
 ];
 
--- src/app/pages/usuarios/cadastro-edicao/cadastro-edicao.component.ts
+++ src/app/pages/usuarios/cadastro-edicao/cadastro-edicao.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-edicao',
   standalone: true,
-  imports: [],
+  imports: [ RouterLink],
   templateUrl: './cadastro-edicao.component.html',
   styleUrl: './cadastro-edicao.component.css'
 })
