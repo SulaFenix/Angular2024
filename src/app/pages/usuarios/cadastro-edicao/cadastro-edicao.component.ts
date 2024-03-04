abf93cce21417703dde2d9c587dd63110d81e90d
Suellen Gularte <sula.fenix@gmail.com>
2024-03-04 20:39:09 -0300

    Aula 22_02

--- src/app/pages/usuarios/cadastro-edicao/cadastro-edicao.component.ts
+++ src/app/pages/usuarios/cadastro-edicao/cadastro-edicao.component.ts
@@ -1,8 +1,11 @@
+import { UsuariosService } from './../../../services/usuarios.service';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { PageTitleComponent } from "../../../components/page-title/page-title.component";
 import { CommonModule } from '@angular/common';
+import { IUsuario } from '../../../interfaces/usuario';
+import Swal from 'sweetalert2';
 
 @Component({
     selector: 'app-cadastro-edicao',
@@ -12,9 +15,34 @@ import { CommonModule } from '@angular/common';
     imports: [ReactiveFormsModule, PageTitleComponent, CommonModule]
 })
 export class CadastroEdicaoUsuariosComponent {
+
+  constructor(private usuariosService: UsuariosService, private router: Router, private route: ActivatedRoute){ }
+
   usuarioForm = new FormGroup({
     nome: new FormControl('', Validators.required),
     idade: new FormControl()
   })
+
+  ngOnInit(){
+    const id = this.route.snapshot.paramMap.get('id');
+  }
+
+  cadastrarUsuarios(){
+    console.log(this.usuarioForm.value);
+    const usuario: IUsuario = this.usuarioForm.value as IUsuario;
+    usuario.ativo = true;
+    this.usuariosService.cadastrarUsuario(usuario).subscribe((result) =>
+    {
+      console.log(result);
+      Swal.fire({
+        title: "Parabéns",
+        text: "Usuário cadastrado com sucesso!",
+        icon: "success"
+      });
+      this.router.navigateByUrl('/usuarios');
+    }, erro => {
+      console.error(erro);
+    });
+  }
 
 }
--- src/app/pages/usuarios/listagem/listagem.component.ts
+++ src/app/pages/usuarios/listagem/listagem.component.ts
@@ -4,6 +4,7 @@ import { UsuariosService } from '../../../services/usuarios.service';
 import { IUsuario } from '../../../interfaces/usuario';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-listagem-usuarios',
@@ -26,6 +27,37 @@ export class ListagemUsuariosComponent {
       this.usuarios = usuarios;
     }, error=> {
       console.error(error);
+    });
+  }
+
+  removerUsuario(id: number){
+    if(id){
+      this.exibirConfirmacao(id);
+    }
+  }
+
+  exibirConfirmacao(id: number){
+    Swal.fire({
+      title: "Tem certeza?",
+      text: "Não tem como desfazer",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sim, remova"
+    }).then((result) => {
+      if (result.isConfirmed){
+        this.usuariosService.romoverUsuario(id).subscribe(result => {
+          this.usuarios.filter(usuarioLista => usuarioLista.id != id)
+          Swal.fire({
+            title: "Removido!",
+            text: "Seu usuário foi removido",
+            icon: "success"
+          });
+        }, erro => {
+          console.error(erro);
+        });
+      }
     });
   }
 }
